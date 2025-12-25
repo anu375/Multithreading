8cffc3c2c8c0a4d97323b6a59a088451ec27b63b
Anushka Jadhav <anujadhav32004@gmail.com>
2025-12-25 11:12:02 +0530

    First Commit

---
+++ src/Multithreading/Fourth_2.java
@@ -0,0 +1,48 @@
+package Multithreading;
+
+public class Fourth_2 
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		Resource res = new Resource();
+		IncreamentThread thread = new IncreamentThread(res);
+		
+		thread.start();
+		thread.join();
+		System.out.println(res.getCounter());
+	}
+
+}
+
+class Resource
+{
+	int counter = 1;
+	
+	public int getCounter()
+	{
+		return this.counter;
+		
+	}
+}
+
+class IncreamentThread extends Thread
+{
+	Resource res;
+	
+	public IncreamentThread(Resource res)
+	{
+		this.res = res;
+	}
+	
+	public void run()
+	{
+		int sum = 1;
+		while(sum<1500)
+		{
+			this.res.counter++;
+			sum++;
+		}
+		System.out.println("total counter : "+this.res.counter);
+	}
+}
